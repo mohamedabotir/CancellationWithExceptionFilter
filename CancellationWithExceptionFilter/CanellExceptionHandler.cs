7d0447f47a8de64bca8212788d6fe8e1531a0902
mohamedabotyear <mohamedabotyear@gmail.com>
2022-12-09 22:34:14 +0200

    add filter  to pipeline to catch cancelled request

---
+++ CancellationWithExceptionFilter/CanellExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CancellationWithExceptionFilter
+{
+    public class CanellExceptionHandler:ExceptionFilterAttribute
+    {
+        private readonly ILogger _logger;
+        public CanellExceptionHandler(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<CanellExceptionHandler>();
+        }
+        public override void OnException(ExceptionContext context)
+        {
+
+            if (context.Exception is OperationCanceledException)
+            {
+                _logger.LogInformation("Request Cancelled");
+                context.ExceptionHandled = true;
+                context.Result = new StatusCodeResult(499);
+            }
+        }
+    }
+}
--- CancellationWithExceptionFilter/Program.cs
+++ CancellationWithExceptionFilter/Program.cs
@@ -1,8 +1,13 @@
+using CancellationWithExceptionFilter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(option =>
+{
+    option.Filters.Add<CanellExceptionHandler>();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
